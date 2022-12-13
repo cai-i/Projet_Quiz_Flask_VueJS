757a8ee21aeab4bda43e1a3e7a2676d1c14c83a0
“Isabelle <isabelle.cai@edu.esiee.fr>
2022-12-13 15:08:39 +0100

    API create project

---
+++ quiz-api/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello_world():
+	x = 'worlddd you'
+	return f"Hello, {x}"
+
+if __name__ == "__main__":
+    app.run()
