4973821a079fb12bb60c1766665a2685b80447ec
“Isabelle <isabelle.cai@edu.esiee.fr>
2023-01-01 10:48:22 +0100

    UI: NavBar more readable

--- quiz-ui/quiz-ui/src/views/HomePage.vue
+++ quiz-ui/quiz-ui/src/views/HomePage.vue
@@ -29,7 +29,7 @@
       <!-- titre text -->
       <span class="font-bold text-red-700">Page d'accueil</span>
     </a>
-    <!-- paragraphe de présentation -->
+    <!-- paragraphes de présentation -->
     <div class="px-12 py-8">
       <p>
         Bienvenue sur notre site dans lequel vous allez pouvoir retrouver un
@@ -46,24 +46,6 @@
         lien de ce quiz
       </p>
     </div>
-    <!-- opération lié au tableau -->
-    <!-- <div class="w-full px-12">
-      <div class="py-4">
-        <label for="table-search" class="sr-only">Search</label>
-        <div class="relative mt-1">
-          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clip-rule="evenodd"></path>
-            </svg>
-          </div>
-          <input type="text" id="table-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items">
-        </div>
-      </div>
-    </div> -->
-    <!-- tableau des scores-->
     <div class="w-full mb-12 px-12">
       <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-red-700 text-white">
         <!-- fond du tableau -->
