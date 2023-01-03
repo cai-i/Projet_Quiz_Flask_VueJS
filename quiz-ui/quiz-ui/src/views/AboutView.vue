6e9876e917ad78d61a8767b762e7f223ff32e282
“Isabelle <isabelle.cai@edu.esiee.fr>
2023-01-03 23:37:58 +0100

    UI: about us : better images links & licences

--- quiz-ui/quiz-ui/src/views/AboutView.vue
+++ quiz-ui/quiz-ui/src/views/AboutView.vue
@@ -12,7 +12,7 @@
     <!-- cartes de présentation des personnes-->
     <div class="w-5/6 mx-auto mt-7 gap-3 justify-center items-center grid grid-cols-1 place-items-center lg:flex">
       <!-- 1ère carte : Huiying DAI-->
-      <div class="group h-96 w-96 [perspective:1000px]">
+      <div class="group grow h-96 w-96 [perspective:1000px]">
         <div class="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
           <div class="absolute inset-0">
             <img class="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40" src="https://img.rawpixel.com/private/static/images/website/2022-05/px1292817-image-kwvy1xs1.jpg?w=1200&h=1200&dpr=1&fit=clip&crop=default&fm=jpg&q=75&vib=3&con=3&usm=15&cs=srgb&bg=F4F4F3&ixlib=js-2.2.1&s=7c30808c57522f732b2b65e71836deb1"/>
@@ -21,7 +21,7 @@
             <div class="flex gap-3 min-h-full flex-col items-center justify-center">
               <h1 class="text-3xl font-bold">Huiying DAI</h1>
               <p class="text-base"> 
-                Etudiante en dernière années d'école d'ingénieur spécialisée en Informatique
+                Etudiante en dernière année d'école d'ingénieur spécialisée en Informatique
               </p>
               <p>
                 Passion : la modélisation 3D et être proactive
@@ -29,12 +29,31 @@
               <a class="mt-2 rounded-full bg-amber-400 text-black py-2 px-3.5 text-sm hover:bg-yellow-600" href="https://fr.linkedin.com/in/huiying-dai-7219a71b5">
                 Linkedin
               </a>
+              <!-- liens image + licence -->
+              <div class="absolute bottom-2 right-3 flex flex-col justify-end text-white">
+                <div class="flex items-center opacity-50 hover:opacity-100">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                  </svg>
+                  <a class="py-1 px-1 hover:underline" href="https://www.rawpixel.com/image/5924500">
+                    image
+                  </a>
+                </div>
+                <div class="flex items-center opacity-50 hover:opacity-100">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                  </svg>
+                  <a class="py-1 px-1 hover:underline" href="https://creativecommons.org/publicdomain/zero/1.0/">
+                    license
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
       <!-- 2ème carte : Emilie CHEN -->
-      <div class="group h-96 w-96 [perspective:1000px]">
+      <div class="group grow h-96 w-96 [perspective:1000px]">
         <div class="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
           <div class="absolute inset-0">
             <img class="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40" src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Algorithmically-generated_landscape_artwork_of_forest_with_Shinto_shrine_using_negative_prompt_for_green_trees.png"/>
@@ -43,7 +62,7 @@
             <div class="flex gap-3 min-h-full flex-col items-center justify-center">
               <h1 class="text-3xl font-bold">Emilie CHEN</h1>
               <p class="text-base"> 
-                Etudiante en dernière années d'école d'ingénieur spécialisée en Informatique
+                Etudiante en dernière année d'école d'ingénieur spécialisée en Informatique
               </p>
               <p>
                 Passion : être créative et ambitieuse
@@ -51,12 +70,31 @@
               <a class="mt-2 rounded-full bg-amber-400 text-black py-2 px-3.5 text-sm hover:bg-yellow-600" href="https://fr.linkedin.com/in/emilie-chen-a077b022a/en?trk=people-guest_people_search-card&original_referer=https%3A%2F%2Fwww.google.com%2F">
                 Linkedin
               </a>
+              <!-- liens image + licence -->
+              <div class="absolute bottom-2 right-3 flex flex-col justify-end text-white">
+                <div class="flex items-center opacity-50 hover:opacity-100">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                  </svg>
+                  <a class="py-1 px-1 hover:underline" href="https://commons.wikimedia.org/wiki/File:Algorithmically-generated_landscape_artwork_of_forest_with_Shinto_shrine_using_negative_prompt_for_green_trees.png">
+                    image
+                  </a>
+                </div>
+                <div class="flex items-center opacity-50 hover:opacity-100">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                  </svg>
+                  <a class="py-1 px-1 hover:underline" href="https://creativecommons.org/licenses/by-sa/4.0/deed.fr">
+                    license
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <!-- 3ère carte : Isabelle AI-->
-      <div class="group h-96 w-96 [perspective:1000px]">
+      <!-- 3ère carte : Isabelle CAI-->
+      <div class="group grow h-96 w-96 [perspective:1000px]">
         <div class="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
           <div class="absolute inset-0">
             <img class="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40" src="https://img.rawpixel.com/private/static/images/website/2022-05/px1292817-image-kwvy1xs1.jpg?w=1200&h=1200&dpr=1&fit=clip&crop=default&fm=jpg&q=75&vib=3&con=3&usm=15&cs=srgb&bg=F4F4F3&ixlib=js-2.2.1&s=7c30808c57522f732b2b65e71836deb1"/>
@@ -65,7 +103,7 @@
             <div class="flex gap-3 min-h-full flex-col items-center justify-center">
               <h1 class="text-3xl font-bold">Isabelle CAI</h1>
               <p class="text-base"> 
-                Etudiante en dernière années d'école d'ingénieur spécialisée en Date Science et Intelligence Artificielle
+                Etudiante en dernière année d'école d'ingénieur spécialisée en Date Science et Intelligence Artificielle
               </p>
               <p>
                 Passion : acquérir de nouvelles connaissances
@@ -73,17 +111,36 @@
               <a class="mt-2 rounded-full bg-amber-400 text-black py-2 px-3.5 text-sm hover:bg-yellow-600" href="https://fr.linkedin.com/in/isabelle-cai-019a0a221">
                 Linkedin
               </a>
+              <!-- liens image + licence -->
+              <div class="absolute bottom-2 right-3 flex flex-col justify-end text-white">
+                <div class="flex items-center opacity-50 hover:opacity-100">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                  </svg>
+                  <a class="py-1 px-1 hover:underline" href="https://www.rawpixel.com/image/5924500">
+                    image
+                  </a>
+                </div>
+                <div class="flex items-center opacity-50 hover:opacity-100">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                  </svg>
+                  <a class="py-1 px-1 hover:underline" href="https://creativecommons.org/publicdomain/zero/1.0/">
+                    license
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
     <!-- carte de présentation du projet et licenses-->
-    <div class="w-5/6 mx-auto py-7 gap-3 justify-center items-center grid grid-cols-1 place-items-center lg:flex">
+    <div class="w-5/6 mx-auto mt-7 gap-3 justify-center items-center grid grid-cols-1 lg:flex">
       <!-- presentation du projet -->
-      <div class="group relative items-center justify-center overflow-hidden hover:shadow-black/30 transition-transform-shadow">
+      <div class="group grow relative items-center justify-center overflow-hidden hover:shadow-black/30 transition-transform-shadow">
         <div class="group h-96 w-full [perspective:1000px] ">
-          <img class="h-full w-full rounded-xl object-cover group-hover:rotate-3 group-hover:scale-125 transition-transform" src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/fl46736410561-image-kybehrff.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=3d55c9025c84ba7dde4908b23b44bafc"/>
+          <img class="h-full w-full rounded-xl object-cover group-hover:rotate-3 group-hover:scale-125 transition-transform" src="https://live.staticflickr.com/1417/5780094040_c29d039c39_b.jpg"/>
         </div>
         <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
         <div class="absolute inset-0 text-white flex flex-col items-center justify-center px-9 text-center translate-y-[63%] group-hover:translate-y-0 transition-all">
@@ -108,16 +165,35 @@
             <a class="underline text-sm text-white hover:text-purple-400" href="https://github.com/cai-i/Projet_Quiz_Flask_VueJS">
               lien
             </a>
+            <!-- liens image + licence -->
+            <div class="absolute bottom-2 right-3 flex flex-col justify-end text-white">
+              <div class="flex items-center opacity-50 hover:opacity-100">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                </svg>
+                <a class="py-1 px-1 hover:underline" href="https://www.flickr.com/photos/stuckincustoms/5780094040">
+                  image
+                </a>
+              </div>
+              <div class="flex items-center opacity-50 hover:opacity-100">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                </svg>
+                <a class="py-1 px-1 hover:underline" href="https://creativecommons.org/licenses/by-nc-sa/2.0/deed.fr">
+                  license
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <!-- licenses -->
-      <div class="group relative items-center justify-center overflow-hidden hover:shadow-black/30 transition-transform-shadow">
-        <div class="h-96 w-96">
-          <img class="h-full w-full rounded-xl object-cover group-hover:rotate-3 group-hover:scale-125 transition-transform" src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/frgreat_wall_china_chinese_10-image-kybdjfx6.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=ac2fb5ab469bd13595f89b987f6761b9"/>
+      <div class="group grow relative items-center justify-center overflow-hidden hover:shadow-black/30 transition-transform-shadow">
+        <div class="h-96 w-full">
+          <img class="h-full w-full rounded-xl object-cover group-hover:rotate-3 group-hover:scale-125 transition-transform" src="https://live.staticflickr.com/7241/26805877601_40d74d62fe_b.jpg"/>
         </div>
         <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-        <div class="absolute inset-0 text-white flex flex-col items-center justify-center px-9 text-center translate-y-[70%] group-hover:translate-y-0 transition-all">
+        <div class="absolute inset-0 mt-3 text-white flex flex-col items-center justify-center px-9 text-center translate-y-[62%] group-hover:translate-y-0 transition-all">
           <h1 class="text-3xl font-bold"> Les images</h1>
           <div class="mb-3 text-gray-200">
             <p class="text-base italic opacity-0 group-hover:opacity-100 transition-opacity duration-300"> 
@@ -128,33 +204,38 @@
             </a>
           </div>
           <p class="font-bold text-green-400"> Les images : </p> 
-          <div class="gap 3 grid grid-cols-2">
-            <div class="gap-7 ">
-              <a class="py-3 px-3 hover:text-yellow-400" href="https://www.rawpixel.com/image/5924500"> 
-                Dragon coloré 
-              </a>
-              <a class="py-3 px-3 hover:text-yellow-400" href="https://commons.wikimedia.org/wiki/File:Algorithmically-generated_landscape_artwork_of_forest_with_Shinto_shrine_using_negative_prompt_for_green_trees.png">
-                Temple rouge
-              </a>
-              <a class="py-3 px-3 hover:text-yellow-400" href="https://www.rawpixel.com/image/6042497/photo-image-desktop-wallpaper-cloud">
-                Nuage rose
-              </a>
-              <a class="py-3 px-3 hover:text-yellow-400" href="https://www.rawpixel.com/image/6036966/photo-image-public-domain-green-nature">
-                Muraille de Chine
+          <!-- <div class="gap 3 grid grid-cols-2"> -->
+            <div class="gap-7">
+              <a class="py-1 px-1 hover:text-yellow-400" href="#">
+                exemple
               </a>
             </div>
-            <div>
-              <a class="py-3 px-3 hover:text-yellow-400" href="https://creativecommons.org/licenses/by-sa/4.0/deed.fr">
-                Temple rouge
+          <!-- </div> -->
+          <div>
+            <p class="font-bold text-green-400"> Autres licences : </p> 
+            <a class="py-1 px-1 hover:text-yellow-400" href="#">
+              exemple
+            </a>
+          </div>
+          <!-- liens image + licence -->
+          <div class="absolute bottom-2 right-3 flex flex-col justify-end text-white">
+            <div class="flex items-center opacity-50 hover:opacity-100">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+              </svg>
+              <a class="py-1 px-1 hover:underline" href="https://www.flickr.com/photos/stuckincustoms/26805877601">
+                image
+              </a>
+            </div>
+            <div class="flex items-center opacity-50 hover:opacity-100">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+              </svg>
+              <a class="py-1 px-1 hover:underline" href="https://creativecommons.org/licenses/by-nc-sa/2.0/deed.fr">
+                license
               </a>
             </div>
           </div>
-          <div>
-              <p class="font-bold text-green-400"> Autres licences : </p> 
-              <a class="py-3 px-3 hover:text-yellow-400" href="https://creativecommons.org/licenses/by-sa/4.0/deed.fr">
-                Temple rouge
-              </a>
-            </div>
         </div>
       </div>
     </div>
