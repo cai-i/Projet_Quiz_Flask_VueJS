fd6843ce0d5278fc8e542ef4961d2d015ee6bef0
“Isabelle <isabelle.cai@edu.esiee.fr>
2023-01-05 14:51:31 +0100

    UI: améliorations avec des composants

---
+++ quiz-ui/quiz-ui/src/views/StaffCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="group grow h-96 w-96 [perspective:1000px]">
+        <div class="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+            <div class="absolute inset-0">
+            <img class="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40" :src="backgroundImageURL"/>
+            </div>
+            <div class="absolute inset-0 h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+                <div class="flex gap-3 min-h-full flex-col items-center justify-center">
+                    <h1 class="text-3xl font-bold">{{ name }}</h1>
+                    <p class="text-base">{{ presentation }}</p>
+                    <p>{{ hobby }}</p>
+                    <a class="mt-2 rounded-full bg-amber-400 text-black py-2 px-3.5 text-sm hover:bg-yellow-600" :href="linkedinURL">
+                    Linkedin
+                    </a>
+                    <!-- liens image + licence -->
+                    <div class="absolute bottom-2 right-3 flex flex-col justify-end text-white">
+                        <div class="flex items-center opacity-50 hover:opacity-100">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                            </svg>
+                            <a class="py-1 px-1 hover:underline" :href="imageURL">
+                            image
+                            </a>
+                        </div>
+                        <div class="flex items-center opacity-50 hover:opacity-100">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                            </svg>
+                            <a class="py-1 px-1 hover:underline" :href="licenseURL">
+                            licence
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['name', 'presentation', 'hobby', 'backgroundImageURL', 'linkedinURL', 'imageURL', 'licenseURL']
+}
+
+</script>
