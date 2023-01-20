d42d701cb61b5dca0cf5a4d6a3e71d6a03acd59a
cai-i <isabelle.cai@edu.esiee.fr>
2023-01-20 17:25:12 +0100

    UI: half done rank handle

--- quiz-ui/src/views/HomePage.vue
+++ quiz-ui/src/views/HomePage.vue
@@ -128,6 +128,20 @@
           style="background-image: url('https://upload.wikimedia.org/wikipedia/commons/c/cc/Celebration_Chinese_Lantern_Festival.jpg');"
         ></div>
       </ScoresTable>
+
+
+
+      <a class="flex items-center gap-2">
+        <svg class="w-6 h-6 min-w-max" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+        </svg>
+        <p> 
+          Attention, le nombre de questions peut changer au fil du 
+          temps, donc ne vous fiez pas uniquement au score.
+        </p>
+      </a>
+
+
     </div>
   </section>
 </template>
